dd727a32b77a3f449ad480ebcd13ba05f6aca739
James Aimonetti <james@2600hz.com>
2017-08-31 19:54:33 +0000

    add link

--- hackerrank/src/arrays_ds.erl
+++ hackerrank/src/arrays_ds.erl
@@ -1,3 +1,4 @@
+%% https://www.hackerrank.com/challenges/arrays-ds
 %% An array is a type of data structure that stores elements of the
 %% same type in a contiguous block of memory. In an array, A, of size
 %% N, each memory location has some unique index, i (where 0 <= i < N),
